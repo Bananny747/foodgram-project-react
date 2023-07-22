e4560ae9669836ca04c34b95a0ac942a48d2c82d
Алексей Филин <alexey.filin92@gmail.com>
2023-07-22 10:27:41 +0300

    Решена проблема авторизации

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,40 +1,40 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
 
 class User(AbstractUser):
     """Кастомная модель пользователя"""
 
-    user = 'user'
-    admin = 'admin'
-    CHOICES = [
-        (user, 'Аутентифицированный пользователь'),
-        (admin, 'Администратор'),
-    ]
-
-    role = models.CharField(
-        choices=CHOICES,
-        default='user',
-        max_length=16,
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name', )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+    )
+    email = models.EmailField(
+        max_length=150,
+        verbose_name='email',
+        unique=True
+    )
+    username = models.CharField(
+        verbose_name='username',
+        max_length=254,
+        unique=True,
+        validators=(UnicodeUsernameValidator(), )
     )
 
-    confirmation_code = models.CharField(
-        max_length=32,
-        null=True,
-    )
+    class Meta:
+        ordering = ('username', )
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
-    def save(self, *args, **kwargs):
-        if self.is_superuser is True:
-            self.role = self.admin
-        super().save(*args, **kwargs)
-
-    @property
-    def is_admin(self):
-        return self.role == self.admin
-
-    @property
-    def is_user(self):
-        return self.role == self.user
+    def __str__(self):
+        return self.username
 
 
 class Follow(models.Model):
