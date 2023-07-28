46b596bbdb61ff0f2be56e9d028e41818b4b4e14
Алексей Филин <alexey.filin92@gmail.com>
2023-07-28 15:52:44 +0300

    Админка для кастомного пользователя: UserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import Follow, User
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
         'username',
         'id',
